d812d0bd175f1438ddadd5e2d8340a35e606fa49
Philippe Parisot <pparisot3@gmail.com>
2022-04-25 23:33:42 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+System.Console.WriteLine(ConvertToMilitaryTime("07:05:45PM"));
+System.Console.WriteLine(ConvertToMilitaryTime("07:05:45AM"));
+System.Console.WriteLine(ConvertToMilitaryTime("12:00:00AM"));
+System.Console.WriteLine(ConvertToMilitaryTime("12:00:00PM"));
+System.Console.WriteLine(ConvertToMilitaryTime("11:59:59PM"));
+
+
+
+static string ConvertToMilitaryTime(string traditionalTime)
+{
+    var strB = new StringBuilder(traditionalTime);
+    _ = strB.Remove(8, 2);
+    int intHour = int.Parse(traditionalTime[..2]);
+
+    if (traditionalTime.Substring(8, 2) == "PM")
+    {
+        if (intHour != 12)
+            intHour += 12;
+        _ = strB.Remove(0, 2);
+        _ = strB.Insert(0, intHour);
+        return strB.ToString();
+    }
+    else
+    {
+        if (intHour == 12)
+        {
+            _ = strB.Remove(0, 2);
+            _ = strB.Insert(0, "00");
+        }
+
+        return strB.ToString();
+    }
+
+}
